662ea65ae752da2d751d2e519b6f53908d118f38
NaveenGowd1 <naveengowd168@gmail.com>
2026-07-05 15:17:40 +0530

    Configure PostgreSQL for production

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,10 +92,10 @@ WSGI_APPLICATION = 'movie_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
