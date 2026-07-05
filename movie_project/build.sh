8d8b18072b062c1ea84b5d17740f29ce51674386
NaveenGowd1 <naveengowd168@gmail.com>
2026-07-05 15:35:40 +0530

    Super user created automatically second attempt

--- movie_project/build.sh
+++ movie_project/build.sh
@@ -5,12 +5,13 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-User.objects.filter(username='admin').exists() or
-User.objects.create_superuser(
-    'admin',
-    'admin@example.com',
-    'YourPassword123'
-)
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin',
+        'admin@example.com',
+        'Admin@12345'
+    )
 "
