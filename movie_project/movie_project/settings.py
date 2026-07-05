f8f117c752b81f12a013f387f458dda09302614c
NaveenGowd1 <naveengowd168@gmail.com>
2026-07-05 14:38:59 +0530

    Prepare backend for Render deployment

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -167,4 +169,3 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
